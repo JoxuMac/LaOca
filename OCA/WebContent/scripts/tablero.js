48d89cf40e9a0abaf5ae74bb83755847185fb2f9
Josue Gutierrez <josue.gutierrez@furiouskoalas.com>
2017-11-10 15:33:55 +0100

    Tablero con casillas 1 a la 63

--- OCA/WebContent/scripts/tablero.js
+++ OCA/WebContent/scripts/tablero.js
@@ -4,7 +4,7 @@ function Tablero() {
 }
 
 Tablero.prototype.dibujar = function(lienzo) {
-	for (var i=0; i<15; i++)
+	for (var i=0; i<63; i++)
 		this.casillas[i].dibujar(lienzo);
 }
 
@@ -18,6 +18,7 @@ Tablero.prototype.crearCasillas = function() {
 		this.casillas[ocas[i]].tipo="OCA";
 	
 
+	var aux = 550;
 	for(var count=0; count<63; count++){
 		
 		// casilla 1
@@ -30,7 +31,7 @@ Tablero.prototype.crearCasillas = function() {
 		}
 		
 		// casillas 2 - 10
-		if(count>0 && count<11){
+		if(count>0 && count<10){
 			this.casillas[count].x0=(count+1)*50; //x
 			this.casillas[count].y0=500; //y
 			this.casillas[count].xF=this.casillas[count].x0+50;
@@ -39,7 +40,7 @@ Tablero.prototype.crearCasillas = function() {
 		}
 		
 		// casillas 11 - 20
-		if(count>9 && count<16){
+		if(count>9 && count<20){
 			this.casillas[count].x0=500; //x
 			this.casillas[count].y0=950-count*50; //y
 			this.casillas[count].xF=this.casillas[count].x0+50;
@@ -48,19 +49,71 @@ Tablero.prototype.crearCasillas = function() {
 		}
 		
 		// casillas 21 - 30
+		if(count>19 && count<30){
+			this.casillas[count].x0=((count*50)-aux); //x
+			aux = aux + 100;
+			this.casillas[count].y0=0; //y
+			this.casillas[count].xF=this.casillas[count].x0+50;
+			this.casillas[count].yF=this.casillas[count].y0+50;
+			this.casillas[count].crearCirculo(50, 50, count);
+			if(aux == 5500){aux = 50}
+		}
 		
 		// casillas 31 - 38
+		if(count>29 && count<38){
+			this.casillas[count].x0=0; //x
+			this.casillas[count].y0=aux-1500; //y
+			this.casillas[count].xF=this.casillas[count].x0+50;
+			this.casillas[count].yF=this.casillas[count].y0+50;
+			this.casillas[count].crearCirculo(50, 50, count);
+			aux = aux + 50;
+		}
 		
 		// casillas 39 - 46
+		if(count>37 && count<46){
+			this.casillas[count].x0=((count+1)*50)-1900; //x
+			this.casillas[count].y0=400; //y
+			this.casillas[count].xF=this.casillas[count].x0+50;
+			this.casillas[count].yF=this.casillas[count].y0+50;
+			this.casillas[count].crearCirculo(50, 50, count);
+		}
 		
 		// casillas 47 - 52
+		if(count>45 && count<52){
+			this.casillas[count].x0=400; //x
+			this.casillas[count].y0=2650-count*50; //y
+			this.casillas[count].xF=this.casillas[count].x0+50;
+			this.casillas[count].yF=this.casillas[count].y0+50;
+			this.casillas[count].crearCirculo(50, 50, count);
+		}
 		
 		// casillas 53 - 58
+		if(count>51 && count<58){
+			this.casillas[count].x0=2950-count*50; //x
+			this.casillas[count].y0=100; //y
+			this.casillas[count].xF=this.casillas[count].x0+50;
+			this.casillas[count].yF=this.casillas[count].y0+50;
+			this.casillas[count].crearCirculo(50, 50, count);
+		}
 		
 		// casillas 59 - 62
+		if(count>57 && count<62){
+			this.casillas[count].x0=100; //x
+			this.casillas[count].y0=aux-1800; //y
+			this.casillas[count].xF=this.casillas[count].x0+50;
+			this.casillas[count].yF=this.casillas[count].y0+50;
+			this.casillas[count].crearCirculo(50, 50, count);
+			aux = aux + 50;
+		}
 		
 		// casillas 63
-
+		if(count>61){
+			this.casillas[count].x0=150; //x
+			this.casillas[count].y0=200; //y
+			this.casillas[count].xF=this.casillas[count].x0+50;
+			this.casillas[count].yF=this.casillas[count].y0+50;
+			this.casillas[count].crearCirculo(200, 150, count);
+		}
 	}
 }
 
