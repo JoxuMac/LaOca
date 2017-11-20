40380803beed29d7147ea209304e9b2568631db6
Josue Gutierrez <josue.gutierrez@furiouskoalas.com>
2017-11-20 12:26:05 +0100

    Actualizacion del tablero y desarollo grafico de funcion dado y ficha

---
+++ OCA/WebContent/scripts/dado.js
@@ -0,0 +1,169 @@
+/**
+ * LA OCA - 2017 - Tecnologias y Sistemas Web 
+ * Escuela Superior de Informatica de Ciudad Real
+ * 
+ * Josue Gutierrez Duran 
+ * Sonia Querencia Martin 
+ * Enrique Simarro Santamaria
+ * Eduardo Fuentes Garcia De Blas
+ */
+
+// Creacion de Dado
+function Dado(x, y) {
+	this.x = x;
+	this.y = y;
+}
+
+// Nueva Tirada de Dado
+Dado.prototype.realizarTidada = function(lienzo) {
+	
+	// Tirada entre 1 y 6
+	var tirada = Math.floor(Math.random() * 6) + 1;
+	
+	console.log(tirada);
+	
+	// Grupo
+	this.g = document.createElementNS("http://www.w3.org/2000/svg", "g");
+
+	// Puntos del dado
+	switch(tirada){
+		case 1:
+			this.g.appendChild(dibujarCentroCentro(this.x, this.y));
+			break;
+		case 2:
+			this.g.appendChild(dibujarCentroIzquierda(this.x, this.y));
+			this.g.appendChild(dibujarCentroDerecha(this.x, this.y));
+			break;
+		case 3:
+			this.g.appendChild(dibujarArribaIzquierda(this.x, this.y));
+			this.g.appendChild(dibujarCentroCentro(this.x, this.y));
+			this.g.appendChild(dibujarAbajoDerecha(this.x, this.y));
+			break;
+		case 4:
+			this.g.appendChild(dibujarArribaIzquierda(this.x, this.y));
+			this.g.appendChild(dibujarArribaDerecha(this.x, this.y));
+			this.g.appendChild(dibujarAbajoIzquierda(this.x, this.y));
+			this.g.appendChild(dibujarAbajoDerecha(this.x, this.y));
+			break;
+		case 5:
+			this.g.appendChild(dibujarArribaIzquierda(this.x, this.y));
+			this.g.appendChild(dibujarArribaDerecha(this.x, this.y));
+			this.g.appendChild(dibujarCentroCentro(this.x, this.y));
+			this.g.appendChild(dibujarAbajoIzquierda(this.x, this.y));
+			this.g.appendChild(dibujarAbajoDerecha(this.x, this.y));
+			break;
+		case 6:
+			this.g.appendChild(dibujarArribaIzquierda(this.x, this.y));
+			this.g.appendChild(dibujarArribaDerecha(this.x, this.y));
+			this.g.appendChild(dibujarCentroIzquierda(this.x, this.y));
+			this.g.appendChild(dibujarCentroDerecha(this.x, this.y));
+			this.g.appendChild(dibujarAbajoIzquierda(this.x, this.y));
+			this.g.appendChild(dibujarAbajoDerecha(this.x, this.y));
+			break;
+	}
+	
+	dibujar(lienzo, this.g);
+}
+
+// Circulo Arriba Derecha
+function dibujarArribaDerecha(x, y){
+	this.circulo = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	this.circulo.setAttribute("cx", x*3/4);
+	this.circulo.setAttribute("cy", y/4);
+	this.circulo.setAttribute("r", 20);
+	this.circulo.setAttribute("width", 20);
+	this.circulo.setAttribute("height", 20);
+	this.circulo.setAttribute("stroke", "black");
+	this.circulo.setAttribute("fill", "white");
+	
+	return this.circulo ;
+}
+
+// Circulo Arriba Izquierda
+function dibujarArribaIzquierda(x, y){
+	this.circulo = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	this.circulo.setAttribute("cx", x/4);
+	this.circulo.setAttribute("cy", y/4);
+	this.circulo.setAttribute("r", 20);
+	this.circulo.setAttribute("width", 20);
+	this.circulo.setAttribute("height", 20);
+	this.circulo.setAttribute("stroke", "black");
+	this.circulo.setAttribute("fill", "white");
+	
+	return this.circulo;
+}
+
+// Circulo Centro Izquierda
+function dibujarCentroIzquierda(x, y){
+	this.circulo = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	this.circulo.setAttribute("cx", x/4);
+	this.circulo.setAttribute("cy", y/2);
+	this.circulo.setAttribute("r", 20);
+	this.circulo.setAttribute("width", 20);
+	this.circulo.setAttribute("height", 20);
+	this.circulo.setAttribute("stroke", "black");
+	this.circulo.setAttribute("fill", "white");
+	
+	return this.circulo;
+}
+
+// Circulo Centro Derecha
+function dibujarCentroDerecha(x, y){
+	this.circulo = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	this.circulo.setAttribute("cx", x*3/4);
+	this.circulo.setAttribute("cy", y/2);
+	this.circulo.setAttribute("r", 20);
+	this.circulo.setAttribute("width", 20);
+	this.circulo.setAttribute("height", 20);
+	this.circulo.setAttribute("stroke", "black");
+	this.circulo.setAttribute("fill", "white");
+	
+	return this.circulo ;
+}
+
+// Circulo Central
+function dibujarCentroCentro(x, y){
+	this.circulo = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	this.circulo.setAttribute("cx", x/2);
+	this.circulo.setAttribute("cy", y/2);
+	this.circulo.setAttribute("r", 20);
+	this.circulo.setAttribute("width", 20);
+	this.circulo.setAttribute("height", 20);
+	this.circulo.setAttribute("stroke", "black");
+	this.circulo.setAttribute("fill", "white");
+	
+	return this.circulo;
+}
+
+// Circulo Abajo Derecha
+function dibujarAbajoDerecha(x, y){
+	this.circulo = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	this.circulo.setAttribute("cx", x*3/4);
+	this.circulo.setAttribute("cy", y*3/4);
+	this.circulo.setAttribute("r", 20);
+	this.circulo.setAttribute("width", 20);
+	this.circulo.setAttribute("height", 20);
+	this.circulo.setAttribute("stroke", "black");
+	this.circulo.setAttribute("fill", "white");
+	
+	return this.circulo;
+}
+
+// Circulo Abajo Izquierda
+function dibujarAbajoIzquierda(x, y){
+	this.circulo = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	this.circulo.setAttribute("cx", x/4);
+	this.circulo.setAttribute("cy", y*3/4);
+	this.circulo.setAttribute("r", 20);
+	this.circulo.setAttribute("width", 20);
+	this.circulo.setAttribute("height", 20);
+	this.circulo.setAttribute("stroke", "black");
+	this.circulo.setAttribute("fill", "white");
+	
+	return this.circulo;
+}
+
+// Dibujar en Lienzo
+function dibujar(lienzo, g) {
+	lienzo.appendChild(g);
+}
---
+++ OCA/WebContent/scripts/ficha.js
@@ -0,0 +1,98 @@
+/**
+ * LA OCA - 2017 - Tecnologias y Sistemas Web 
+ * Escuela Superior de Informatica de Ciudad Real
+ * 
+ * Josue Gutierrez Duran 
+ * Sonia Querencia Martin 
+ * Enrique Simarro Santamaria
+ * Eduardo Fuentes Garcia De Blas
+ */
+
+// Creacion de Ficha
+function Ficha(idficha, lienzo) {
+	this.idficha = idficha;
+	switch(this.idficha){
+		case 1:
+			pintarFicha1(lienzo);
+			break;
+		case 2:
+			pintarFicha2(lienzo);
+			break;
+		case 3:
+			pintarFicha3(lienzo);
+			break;
+		case 4:
+			pintarFicha4(lienzo);
+			break;
+	}
+}
+
+// Nueva Tirada de Dado
+Ficha.prototype.moverFicha = function(lienzo, tiradanueva, idficha) {
+	
+	this.idcasilla += tirada;
+	
+	console.log(this.idcasilla);
+	
+	this.g = document.createElementNS("http://www.w3.org/2000/svg", "g");
+	
+
+	// blue
+	// green
+	// purple
+	//red
+	
+	//this.g.appendChild(this.circulo);
+	
+	dibujar(lienzo, this.circulo);
+}
+
+//Pintar Ficha 1
+function pintarFicha1(lienzo){
+	this.circulo = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	this.circulo.setAttribute("cx", 15);
+	this.circulo.setAttribute("cy", 530);
+	this.circulo.setAttribute("r", 10);
+	this.circulo.setAttribute("stroke", "black");
+	this.circulo.setAttribute("fill", "purple");
+	dibujar(lienzo, this.circulo);
+}
+
+//Pintar Ficha 2
+function pintarFicha2(lienzo){
+	this.circulo = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	this.circulo.setAttribute("cx", 38);
+	this.circulo.setAttribute("cy", 530);
+	this.circulo.setAttribute("r", 10);
+	this.circulo.setAttribute("stroke", "black");
+	this.circulo.setAttribute("fill", "red");
+	dibujar(lienzo, this.circulo);
+}
+
+//Pintar Ficha 3
+function pintarFicha3(lienzo){
+	this.circulo = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	this.circulo.setAttribute("cx", 61);
+	this.circulo.setAttribute("cy", 530);
+	this.circulo.setAttribute("r", 10);
+	this.circulo.setAttribute("stroke", "black");
+	this.circulo.setAttribute("fill", "blue");
+	dibujar(lienzo, this.circulo);
+}
+
+// Pintar Ficha 4
+function pintarFicha4(lienzo){
+	this.circulo = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	this.circulo.setAttribute("cx", 84);
+	this.circulo.setAttribute("cy", 530);
+	this.circulo.setAttribute("r", 10);
+	this.circulo.setAttribute("stroke", "black");
+	this.circulo.setAttribute("fill", "green");
+	dibujar(lienzo, this.circulo);
+}
+
+// Dibujar en Lienzo
+function dibujar(lienzo, g) {
+	lienzo.appendChild(g);
+	console.log("hola");
+}
